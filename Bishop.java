760038c0f6632aa6e780ad9ffe5caf874dfa88d3
Colin Quick <quickcj@s.dscdk12.org>
2013-05-09 08:52:56 -0600

    bishop code

---
+++ Bishop.java
@@ -0,0 +1,48 @@
+public class Bishop extends Piece {
+
+	private boolean onBoard;
+
+	public Bishop() {
+
+	}
+
+	
+	public ArrayList<Postion> getMovePositions() {
+		int i = 1;
+		ArrayList<Postion> possibleMoves = new ArrayList<Postion>;
+		/*
+		Upper Right Positions
+		 */
+		while(getPosistion(x+i, y+i) != null) {
+			possibleMoves.add(getPosition(x+i, y+1));
+			i++;
+		}
+		/*
+		Upper Left Positions
+		 */
+		while(getPosistion(x-i, y+i) != null) {
+			possibleMoves.add(getPosition(x-i, y+1));
+			i++;
+		}
+		/*
+		Lower Right Positions
+		 */
+		while(getPosistion(x+i, y-i) != null) {
+			possibleMoves.add(getPosition(x+i, y-1));
+			i++;
+		}
+		/*
+		Lower Left Positions
+		 */
+		while(getPosistion(x-i, y-i) != null) {
+			possibleMoves.add(getPosition(x-i, y-1));
+			i++;
+		}
+	return possibleMoves;
+	}
+
+	public void makeMove() {
+
+	}
+	
+}
